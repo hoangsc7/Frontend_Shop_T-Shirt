9b6e387c6e6b09cdaeae16805dcd386380e1606c
hoangsc7 <hoangsc7@gmail.com>
2025-04-09 20:50:16 +0700

    up5

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-// axios.defaults.baseURL = process.env.URL_BE
-axios.defaults.baseURL = 'backend-shop-t-shirt.onrender.com'
+axios.defaults.baseURL = process.env.VUE_APP_URL_BE
+console.log('Base URL:', process.env.VUE_APP_URL_BE)
+
+// axios.defaults.baseURL = 'backend-shop-t-shirt.onrender.com'
 createApp(App).use(store).use(router).mount('#app')
